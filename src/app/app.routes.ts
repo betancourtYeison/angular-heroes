615e4b35370612f4f8910c4c3800de5e2f9035b2
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-05-13 16:43:53 -0500

    Update routes to use hash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,5 +14,5 @@ const APP_ROUTES: Routes = [
   { path: "**", pathMatch: "full", redirectTo: "home" }
 ];
 
-// export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
+// export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
